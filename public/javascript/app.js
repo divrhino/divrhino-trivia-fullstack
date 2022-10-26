305b413b01bdc51b4997656d111452367027a080
Div Rhino <fancydresspenguin+divrhino@gmail.com>
2022-10-26 21:07:02 +0800

    Add pre-prepared JavaScript

---
+++ public/javascript/app.js
@@ -0,0 +1,16 @@
+// Add click event to fact to show answer
+(() => {
+    const answerWrapper = document.querySelectorAll('.answer-wrapper');
+    const toggleBtns = document.querySelectorAll('.answer-toggle')
+
+    for (const ans of answerWrapper) {
+        ans.style.display = 'none';
+    }
+
+    for (const btn of toggleBtns) {
+        btn.addEventListener('click', (e) => {
+            const answer = e.target.parentElement.nextElementSibling;
+            answer.style.display = answer.style.display === 'none' ? 'block' : 'none';
+        } );
+    }
+})()
